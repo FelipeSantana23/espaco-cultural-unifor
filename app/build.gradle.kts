06e38b023d21585f75ab19ba359c9b56c2b4685e
Luiz Felipe Gomes Santana <110501530+FelipeSantana23@users.noreply.github.com>
2024-10-21 18:30:26 -0300

    first commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
-    // O plugin do Google Services deve ser aplicado apenas uma vez no final
 }
 
 android {
@@ -27,32 +26,22 @@ android {
             )
         }
     }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-
     kotlinOptions {
         jvmTarget = "1.8"
     }
 }
 
 dependencies {
-    // Escolha uma das bibliotecas:
-    implementation 'com.google.firebase:firebase-database-ktx:20.3.0' // Para Realtime Database
-    // implementation 'com.google.firebase:firebase-firestore-ktx:24.4.0' // Para Firestore
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
-
-// Plugin do Google Services deve ser aplicado aqui, após as dependências
-apply(plugin: 'com.google.gms.google-services')
